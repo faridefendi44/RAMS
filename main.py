a8f842beec1756eaa74ff5aa7af47e131cbbe179
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-06-19 09:35:08 +0700

    dependencies

--- main.py
+++ main.py
@@ -6,22 +6,19 @@ import numpy as np
 from utils import get_csv_url
 from plot import plot_interactive_weibull
 from data_processing import process_failure_rate, process_availability
+from dotenv import load_dotenv
+import os
 
-# URL default Google Sheets
-url_default = "https://docs.google.com/spreadsheets/d/1toTRK3kANu7lL7BuNcNT37H4I3ztnYNi/edit?usp=sharing&ouid=111987242205368293051&rtpof=true&sd=true"
+load_dotenv()
 
-# Input URL Google Sheets di sidebar
+url_default = os.getenv('GOOGLE_SHEETS_URL')
+
 st.sidebar.header('URL Google Sheets')
 url = st.sidebar.text_input('Masukkan URL Google Sheets:', url_default)
-
-# Konversi URL menjadi format CSV eksport
 csv_url = get_csv_url(url)
 
 try:
-    # Baca data dari Google Sheets
     data = pd.read_csv(csv_url, thousands=',', decimal='.')
-
-    # Sidebar untuk memilih metrik
     st.sidebar.header('Pilih Metrik')
     metric = st.sidebar.radio('Metrik:', ('Failure Rate', 'Availability'))
 
