a0e4c17e09455f6409cba13eff287d0af07aee7b
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-06-20 10:37:00 +0700

    url

--- main.py
+++ main.py
@@ -4,36 +4,35 @@ from datetime import datetime
 import numpy as np
 from dotenv import load_dotenv
 import os
-
 from utils import get_csv_url
 from plot import plot_interactive_weibull
 from data_processing import process_failure_rate, process_availability
 
-# Load variabel lingkungan dari file .env
-load_dotenv()
-
-# URL default dari file .env
-url_default = "https://docs.google.com/spreadsheets/d/1toTRK3kANu7lL7BuNcNT37H4I3ztnYNi/edit?usp=sharing&ouid=111987242205368293051&rtpof=true&sd=true"
+url_default = ""
 
 # Input URL Google Sheets di sidebar
 st.sidebar.header('URL Google Sheets')
 url = st.sidebar.text_input('Masukkan URL Google Sheets:', url_default)
 
-# Konversi URL menjadi format CSV eksport
-csv_url = get_csv_url(url)
+if url.strip() == "":
+    st.error("Harap masukkan URL spreadsheet yang valid.")
+else:
+    # Konversi URL menjadi format CSV eksport
+    csv_url = get_csv_url(url)
 
-try:
-    # Baca data dari Google Sheets
-    data = pd.read_csv(csv_url, thousands=',', decimal='.')
+    try:
+        # Baca data dari Google Sheets
+        data = pd.read_csv(csv_url, thousands=',', decimal='.')
 
-    # Sidebar untuk memilih metrik
-    st.sidebar.header('Pilih Metrik')
-    metric = st.sidebar.radio('Metrik:', ('Failure Rate', 'Availability'))
+        # Sidebar untuk memilih metrik
+        st.sidebar.header('Pilih Metrik')
+        metric = st.sidebar.radio('Metrik:', ('Failure Rate', 'Availability'))
 
-    if metric == 'Failure Rate':
-        process_failure_rate(data)
-    elif metric == 'Availability':
-        process_availability(data)
+        if metric == 'Failure Rate':
+            process_failure_rate(data)
+        elif metric == 'Availability':
+            process_availability(data)
 
-except Exception as e:
-    st.error(f"Error reading data: {e}")
+    except Exception as e:
+        st.error(f"Error reading data: {e}")
+
